f9e3c966fe4abb68210115923c7de7d70b7740e7
HandMeTheCode <102748051+HandMeTheCode@users.noreply.github.com>
2023-10-12 18:30:11 -0500

    Add files via upload
    form1.vb is the file that holds the javascript (?), check it future me idk remember neither

---
+++ WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,435 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        btnSTART = New Button()
+        btnDelete = New Button()
+        btnDeleteAll = New Button()
+        btnNext = New Button()
+        btnRN1 = New Button()
+        btnRN2 = New Button()
+        btnRN3 = New Button()
+        btnRN4 = New Button()
+        btnDIVIDE = New Button()
+        btnPLUS = New Button()
+        btnFOR = New Button()
+        btnMINUS = New Button()
+        txtbxN1 = New TextBox()
+        txtbxO1 = New TextBox()
+        txtbxN2 = New TextBox()
+        txtbxO2 = New TextBox()
+        txtbxN3 = New TextBox()
+        txtbxO3 = New TextBox()
+        txtbxN4 = New TextBox()
+        txtbxSYSTRESULT = New TextBox()
+        Label1 = New Label()
+        btnSolve = New Button()
+        TextBox10 = New TextBox()
+        txtbxUSERRESULT = New TextBox()
+        Label3 = New Label()
+        Label2 = New Label()
+        Timer1 = New Timer(components)
+        TextBox11 = New TextBox()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        SuspendLayout()
+        ' 
+        ' btnSTART
+        ' 
+        btnSTART.Location = New Point(960, 93)
+        btnSTART.Name = "btnSTART"
+        btnSTART.Size = New Size(75, 23)
+        btnSTART.TabIndex = 0
+        btnSTART.Tag = "btnSTART"
+        btnSTART.Text = "INICIO"
+        btnSTART.UseVisualStyleBackColor = True
+        ' 
+        ' btnDelete
+        ' 
+        btnDelete.Location = New Point(960, 171)
+        btnDelete.Name = "btnDelete"
+        btnDelete.Size = New Size(75, 23)
+        btnDelete.TabIndex = 1
+        btnDelete.Tag = "btnDelete "
+        btnDelete.Text = "BORRAR"
+        btnDelete.UseVisualStyleBackColor = True
+        ' 
+        ' btnDeleteAll
+        ' 
+        btnDeleteAll.Location = New Point(960, 200)
+        btnDeleteAll.Name = "btnDeleteAll"
+        btnDeleteAll.Size = New Size(75, 38)
+        btnDeleteAll.TabIndex = 2
+        btnDeleteAll.Tag = "btnDeleteAll"
+        btnDeleteAll.Text = "BORRAR TODO"
+        btnDeleteAll.UseVisualStyleBackColor = True
+        ' 
+        ' btnNext
+        ' 
+        btnNext.Location = New Point(960, 270)
+        btnNext.Name = "btnNext"
+        btnNext.Size = New Size(75, 23)
+        btnNext.TabIndex = 3
+        btnNext.Tag = "btnNext"
+        btnNext.Text = "SIGUIENTE"
+        btnNext.UseVisualStyleBackColor = True
+        ' 
+        ' btnRN1
+        ' 
+        btnRN1.Location = New Point(447, 208)
+        btnRN1.Name = "btnRN1"
+        btnRN1.Size = New Size(75, 23)
+        btnRN1.TabIndex = 4
+        btnRN1.Tag = "btnRN1"
+        btnRN1.UseVisualStyleBackColor = True
+        ' 
+        ' btnRN2
+        ' 
+        btnRN2.Location = New Point(528, 208)
+        btnRN2.Name = "btnRN2"
+        btnRN2.Size = New Size(75, 23)
+        btnRN2.TabIndex = 5
+        btnRN2.Tag = "btnRN2"
+        btnRN2.UseVisualStyleBackColor = True
+        ' 
+        ' btnRN3
+        ' 
+        btnRN3.Location = New Point(609, 208)
+        btnRN3.Name = "btnRN3"
+        btnRN3.Size = New Size(75, 23)
+        btnRN3.TabIndex = 6
+        btnRN3.Tag = "btnRN3"
+        btnRN3.UseVisualStyleBackColor = True
+        ' 
+        ' btnRN4
+        ' 
+        btnRN4.Location = New Point(690, 208)
+        btnRN4.Name = "btnRN4"
+        btnRN4.Size = New Size(75, 23)
+        btnRN4.TabIndex = 7
+        btnRN4.Tag = "btnRN4"
+        btnRN4.UseVisualStyleBackColor = True
+        ' 
+        ' btnDIVIDE
+        ' 
+        btnDIVIDE.Location = New Point(652, 255)
+        btnDIVIDE.Name = "btnDIVIDE"
+        btnDIVIDE.Size = New Size(37, 23)
+        btnDIVIDE.TabIndex = 8
+        btnDIVIDE.Tag = "btnDIVIDE"
+        btnDIVIDE.Text = "/"
+        btnDIVIDE.UseVisualStyleBackColor = True
+        ' 
+        ' btnPLUS
+        ' 
+        btnPLUS.Location = New Point(523, 255)
+        btnPLUS.Name = "btnPLUS"
+        btnPLUS.Size = New Size(37, 23)
+        btnPLUS.TabIndex = 9
+        btnPLUS.Tag = "btnPLUS"
+        btnPLUS.Text = "+"
+        btnPLUS.UseVisualStyleBackColor = True
+        ' 
+        ' btnFOR
+        ' 
+        btnFOR.Location = New Point(609, 255)
+        btnFOR.Name = "btnFOR"
+        btnFOR.Size = New Size(37, 23)
+        btnFOR.TabIndex = 10
+        btnFOR.Tag = "btnFOR"
+        btnFOR.Text = "*"
+        btnFOR.UseVisualStyleBackColor = True
+        ' 
+        ' btnMINUS
+        ' 
+        btnMINUS.Location = New Point(566, 255)
+        btnMINUS.Name = "btnMINUS"
+        btnMINUS.Size = New Size(37, 23)
+        btnMINUS.TabIndex = 11
+        btnMINUS.Tag = "btnMINUS"
+        btnMINUS.Text = "-"
+        btnMINUS.UseVisualStyleBackColor = True
+        ' 
+        ' txtbxN1
+        ' 
+        txtbxN1.Location = New Point(428, 138)
+        txtbxN1.Name = "txtbxN1"
+        txtbxN1.Size = New Size(43, 23)
+        txtbxN1.TabIndex = 12
+        txtbxN1.Tag = "txtbxN"
+        ' 
+        ' txtbxO1
+        ' 
+        txtbxO1.Location = New Point(477, 138)
+        txtbxO1.Name = "txtbxO1"
+        txtbxO1.Size = New Size(43, 23)
+        txtbxO1.TabIndex = 13
+        txtbxO1.Tag = "txtbxO"
+        ' 
+        ' txtbxN2
+        ' 
+        txtbxN2.Location = New Point(526, 137)
+        txtbxN2.Name = "txtbxN2"
+        txtbxN2.Size = New Size(43, 23)
+        txtbxN2.TabIndex = 14
+        txtbxN2.Tag = "txtbxN"
+        ' 
+        ' txtbxO2
+        ' 
+        txtbxO2.Location = New Point(575, 137)
+        txtbxO2.Name = "txtbxO2"
+        txtbxO2.Size = New Size(43, 23)
+        txtbxO2.TabIndex = 15
+        txtbxO2.Tag = "txtbxO"
+        ' 
+        ' txtbxN3
+        ' 
+        txtbxN3.Location = New Point(624, 138)
+        txtbxN3.Name = "txtbxN3"
+        txtbxN3.Size = New Size(43, 23)
+        txtbxN3.TabIndex = 16
+        txtbxN3.Tag = "txtbxN"
+        ' 
+        ' txtbxO3
+        ' 
+        txtbxO3.Location = New Point(673, 138)
+        txtbxO3.Name = "txtbxO3"
+        txtbxO3.Size = New Size(43, 23)
+        txtbxO3.TabIndex = 17
+        txtbxO3.Tag = "txtbxO"
+        ' 
+        ' txtbxN4
+        ' 
+        txtbxN4.Location = New Point(722, 138)
+        txtbxN4.Name = "txtbxN4"
+        txtbxN4.Size = New Size(43, 23)
+        txtbxN4.TabIndex = 18
+        txtbxN4.Tag = "txtbxN"
+        ' 
+        ' txtbxSYSTRESULT
+        ' 
+        txtbxSYSTRESULT.Location = New Point(792, 94)
+        txtbxSYSTRESULT.Name = "txtbxSYSTRESULT"
+        txtbxSYSTRESULT.ReadOnly = True
+        txtbxSYSTRESULT.Size = New Size(100, 23)
+        txtbxSYSTRESULT.TabIndex = 19
+        txtbxSYSTRESULT.Tag = "txtbxSYSTRESULT"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(771, 140)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(15, 15)
+        Label1.TabIndex = 20
+        Label1.Text = "="
+        ' 
+        ' btnSolve
+        ' 
+        btnSolve.Location = New Point(960, 132)
+        btnSolve.Name = "btnSolve"
+        btnSolve.Size = New Size(75, 23)
+        btnSolve.TabIndex = 23
+        btnSolve.Tag = "btnSolve "
+        btnSolve.Text = "RESOLVER"
+        btnSolve.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox10
+        ' 
+        TextBox10.Location = New Point(1062, 20)
+        TextBox10.Name = "TextBox10"
+        TextBox10.ReadOnly = True
+        TextBox10.Size = New Size(46, 23)
+        TextBox10.TabIndex = 25
+        TextBox10.Tag = "TextBox10"
+        ' 
+        ' txtbxUSERRESULT
+        ' 
+        txtbxUSERRESULT.Location = New Point(792, 138)
+        txtbxUSERRESULT.Name = "txtbxUSERRESULT"
+        txtbxUSERRESULT.ReadOnly = True
+        txtbxUSERRESULT.Size = New Size(100, 23)
+        txtbxUSERRESULT.TabIndex = 27
+        txtbxUSERRESULT.Tag = "txtbxUSERRESULT"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(665, 97)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(121, 15)
+        Label3.TabIndex = 28
+        Label3.Text = "Resultado esperado ="
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(951, 28)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(105, 15)
+        Label2.TabIndex = 29
+        Label2.Text = "Puntaje LLevado ="
+        ' 
+        ' Timer1
+        ' 
+        Timer1.Enabled = True
+        Timer1.Interval = 1000
+        ' 
+        ' TextBox11
+        ' 
+        TextBox11.Location = New Point(899, 20)
+        TextBox11.Name = "TextBox11"
+        TextBox11.ReadOnly = True
+        TextBox11.Size = New Size(46, 23)
+        TextBox11.TabIndex = 30
+        TextBox11.Tag = "TextBox11"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(849, 23)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(44, 15)
+        Label4.TabIndex = 31
+        Label4.Text = "Reloj ="
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(12, 9)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(330, 30)
+        Label5.TabIndex = 32
+        Label5.Text = "UNIVERSIDAD TECNOLÓGICA DE PANAMÁ" & vbCrLf & "FACULTAD DE INGENIERÍA DE SISTEMAS COMPUTACIONALES" & vbCrLf
+        Label5.TextAlign = ContentAlignment.TopCenter
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(2, 41)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(355, 45)
+        Label6.TabIndex = 33
+        Label6.Text = "DEPARTAMENTO DE COMPUTACION Y SIMULACION DE SISTEMAS" & vbCrLf & vbCrLf & "Licenciatura en Desarrollo de Software" & vbCrLf
+        Label6.TextAlign = ContentAlignment.TopCenter
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(99, 97)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(144, 15)
+        Label7.TabIndex = 34
+        Label7.Text = "Desarrollo de Software VIII" & vbCrLf
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(98, 132)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(145, 45)
+        Label8.TabIndex = 35
+        Label8.Text = "Integrantes: " & vbCrLf & "Nombre: Cristian Andrade" & vbCrLf & "Cedula: 3-749-573"
+        Label8.TextAlign = ContentAlignment.TopCenter
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1128, 549)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(TextBox11)
+        Controls.Add(Label2)
+        Controls.Add(Label3)
+        Controls.Add(txtbxUSERRESULT)
+        Controls.Add(TextBox10)
+        Controls.Add(btnSolve)
+        Controls.Add(Label1)
+        Controls.Add(txtbxSYSTRESULT)
+        Controls.Add(txtbxN4)
+        Controls.Add(txtbxO3)
+        Controls.Add(txtbxN3)
+        Controls.Add(txtbxO2)
+        Controls.Add(txtbxN2)
+        Controls.Add(txtbxO1)
+        Controls.Add(txtbxN1)
+        Controls.Add(btnMINUS)
+        Controls.Add(btnFOR)
+        Controls.Add(btnPLUS)
+        Controls.Add(btnDIVIDE)
+        Controls.Add(btnRN4)
+        Controls.Add(btnRN3)
+        Controls.Add(btnRN2)
+        Controls.Add(btnRN1)
+        Controls.Add(btnNext)
+        Controls.Add(btnDeleteAll)
+        Controls.Add(btnDelete)
+        Controls.Add(btnSTART)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnSTART As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnDeleteAll As Button
+    Friend WithEvents btnNext As Button
+    Friend WithEvents btnRN1 As Button
+    Friend WithEvents btnRN2 As Button
+    Friend WithEvents btnRN3 As Button
+    Friend WithEvents btnRN4 As Button
+    Friend WithEvents btnDIVIDE As Button
+    Friend WithEvents btnPLUS As Button
+    Friend WithEvents btnFOR As Button
+    Friend WithEvents btnMINUS As Button
+    Friend WithEvents txtbxN1 As TextBox
+    Friend WithEvents txtbxO1 As TextBox
+    Friend WithEvents txtbxN2 As TextBox
+    Friend WithEvents txtbxO2 As TextBox
+    Friend WithEvents txtbxN3 As TextBox
+    Friend WithEvents txtbxO3 As TextBox
+    Friend WithEvents txtbxN4 As TextBox
+    Friend WithEvents txtbxSYSTRESULT As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnSolve As Button
+    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents txtbxUSERRESULT As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents TextBox11 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+End Class
---
+++ WinFormsApp1/Form1.vb
@@ -0,0 +1,379 @@
+﻿Imports System.Threading
+
+Public Class Form1
+    Private rand As New Random()
+    Private txtbxNArray As New List(Of TextBox)()
+    Private txtbxOArray As New List(Of TextBox)()
+    Private btnArray As New List(Of Button)()
+    Private sysResult As Double ' Track the Resultado Matematico
+    Private textBox10Count As Integer = 0 ' Initialize TextBox10 count
+    Private countdownTime As Integer = 60 ' Initialize countdown time to 60 seconds
+    Private isCountdownRunning As Boolean = False ' Track if the countdown timer is running
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Adds text boxes to the txtbxNArray
+        txtbxNArray.Add(txtbxN1)
+        txtbxNArray.Add(txtbxN2)
+        txtbxNArray.Add(txtbxN3)
+        txtbxNArray.Add(txtbxN4)
+
+        ' Adds text boxes to the txtbxOArray
+        txtbxOArray.Add(txtbxO1)
+        txtbxOArray.Add(txtbxO2)
+        txtbxOArray.Add(txtbxO3)
+
+        ' Adds buttons to the btnArray
+        btnArray.Add(btnRN1)
+        btnArray.Add(btnRN2)
+        btnArray.Add(btnRN3)
+        btnArray.Add(btnRN4)
+
+        ' Disable buttons, text boxes, and other controls
+        DisableControls()
+
+        ' Initialize Timer1
+        Timer1.Interval = 1000 ' 1 second
+        Timer1.Enabled = False ' Initially, the timer is disabled
+    End Sub
+
+    ' Handle the Tick event of Timer1
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If countdownTime > 0 Then
+            countdownTime -= 1
+            TextBox11.Text = countdownTime.ToString()
+        Else
+            ' Countdown has reached 0
+            Timer1.Enabled = False ' Disable the timer
+            DisableControls() ' Disable all controls
+            countdownTime = 60 ' Reset countdown to 60 seconds
+            TextBox11.Text = countdownTime.ToString()
+            btnSTART.Enabled = True ' Enable btnSTART
+        End If
+    End Sub
+
+    Private Sub DisableControls()
+        ' Disable buttons in array "a" (txtbxN1, txtbxO1, txtbxN2, txtbxO2, txtbxN3, txtbxO3, txtbxN4)
+        For Each txtbx In txtbxNArray
+            txtbx.Enabled = False
+        Next
+
+        For Each txtbx In txtbxOArray
+            txtbx.Enabled = False
+        Next
+
+        ' Disable buttons in array "b" (btnRN1, btnRN2, btnRN3, btnRN4)
+        For Each btn In btnArray
+            btn.Enabled = False
+        Next
+
+        ' Disable buttons in array "c" (btnPLUS, btnMINUS, btnFOR, btnDIVIDE)
+        btnPLUS.Enabled = False
+        btnMINUS.Enabled = False
+        btnFOR.Enabled = False
+        btnDIVIDE.Enabled = False
+
+        ' Disable other controls
+        txtbxSYSTRESULT.Enabled = False
+        txtbxUSERRESULT.Enabled = False
+        TextBox10.Enabled = False
+        TextBox11.Enabled = False
+        btnDelete.Enabled = False
+        btnDeleteAll.Enabled = False
+        btnNext.Enabled = False
+        btnSolve.Enabled = False
+
+
+    End Sub
+
+    Private Sub EnableControls()
+        ' Enable buttons in array "b" (btnRN1, btnRN2, btnRN3, btnRN4)
+        For Each btn In btnArray
+            btn.Enabled = True
+        Next
+
+        ' Enable btnDELETE, btnDELETEALL, btnNEXT, and btnSOLVE
+        btnDelete.Enabled = True
+        btnDeleteAll.Enabled = True
+        btnNext.Enabled = True
+        btnSolve.Enabled = True
+    End Sub
+
+    Private Sub btnSTART_Click(sender As Object, e As EventArgs) Handles btnSTART.Click
+        ' Start the countdown timer
+        Timer1.Enabled = True
+        If Not isCountdownRunning Then
+            ' Start the countdown timer
+            Timer1.Enabled = True
+            isCountdownRunning = True
+        End If
+
+        ' Clear data from array "a" (txtbxNArray)
+        For Each txtbx In txtbxNArray
+            txtbx.Text = ""
+        Next
+
+        ' Clear data from array "a" (txtbxOArray)
+        For Each txtbx In txtbxOArray
+            txtbx.Text = ""
+        Next
+
+        ' Clear txtbxUSERRESULT
+        TextBox10.Text = ""
+
+        ' Clear txtbxSYSTRESULT
+        txtbxSYSTRESULT.Text = ""
+
+        ' Clear txtbxUSERRESULT
+        txtbxUSERRESULT.Text = ""
+
+        ' Generate random numbers for each button in array "b" (btnRN1, btnRN2, btnRN3, btnRN4)
+        For Each btn In btnArray
+            Dim randomValue As Integer = rand.Next(1, 10) ' Range (1, 10)
+            btn.Text = randomValue.ToString()
+
+            ' Accumulate the random values to calculate the mathematical result
+            sysResult += randomValue
+        Next
+
+        ' Display the mathematical result in txtbxSYSTRESULT
+        txtbxSYSTRESULT.Text = sysResult.ToString()
+
+        ' Enable controls for user interaction
+        EnableControls()
+
+        ' Disable btnSTART
+        btnSTART.Enabled = False
+
+        ' Display btnNEXT
+        btnNext.Enabled = False
+    End Sub
+
+    Private Sub btnRN_Click(sender As Object, e As EventArgs) Handles btnRN1.Click, btnRN2.Click, btnRN3.Click, btnRN4.Click
+        Dim clickedButton As Button = CType(sender, Button)
+
+        ' Find the first available txtbxN and display the number
+        For Each txtbx In txtbxNArray
+            If txtbx.Text = "" Then
+                txtbx.Text = clickedButton.Text
+                clickedButton.Enabled = False
+                Exit For
+            End If
+        Next
+
+        ' Enable buttons in array "c" (btnPLUS, btnMINUS, btnFOR, btnDIVIDE)
+        btnPLUS.Enabled = True
+        btnMINUS.Enabled = True
+        btnFOR.Enabled = True
+        btnDIVIDE.Enabled = True
+    End Sub
+
+    Private Sub btnOperator_Click(sender As Object, e As EventArgs) Handles btnPLUS.Click, btnMINUS.Click, btnFOR.Click, btnDIVIDE.Click
+        Dim clickedButton As Button = CType(sender, Button)
+
+        ' Find the first available txtbxO and display the operator
+        For Each txtbx In txtbxOArray
+            If txtbx.Text = "" Then
+                txtbx.Text = clickedButton.Text
+                clickedButton.Enabled = False
+                Exit For
+            End If
+        Next
+    End Sub
+
+    Private Sub btnDELETE_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        Dim i As Integer = 0
+
+        ' Find the last filled txtbxN and clear it
+        For i = txtbxNArray.Count - 1 To 0 Step -1
+            If txtbxNArray(i).Text <> "" Then
+                txtbxNArray(i).Text = ""
+                btnArray(i).Enabled = True ' Enable the corresponding btnRN
+                Exit For
+            End If
+        Next
+
+        ' Enable the corresponding btnRN for the cleared txtbxN
+        If i >= 0 AndAlso i < btnArray.Count Then
+            btnArray(i).Enabled = True
+        End If
+
+        ' Find the last filled txtbxO and clear it
+        For i = txtbxOArray.Count - 1 To 0 Step -1
+            If txtbxOArray(i).Text <> "" Then
+                txtbxOArray(i).Text = ""
+                Exit For
+            End If
+        Next
+
+        ' Enable buttons in array "c" (btnPLUS, btnMINUS, btnFOR, btnDIVIDE)
+        btnPLUS.Enabled = True
+        btnMINUS.Enabled = True
+        btnFOR.Enabled = True
+        btnDIVIDE.Enabled = True
+    End Sub
+
+    Private Sub btnDELETEALL_Click(sender As Object, e As EventArgs) Handles btnDeleteAll.Click
+        ' Clear all txtbxO elements and enable them
+        For i = 0 To txtbxOArray.Count - 1
+            txtbxOArray(i).Text = ""
+            btnArray(i).Enabled = True ' Enable the corresponding btnRN
+        Next
+
+        For i = 0 To txtbxNArray.Count - 1
+            txtbxNArray(i).Text = ""
+            btnArray(i).Enabled = True ' Enable the corresponding btnRN
+        Next
+    End Sub
+
+    Private Sub btnNEXT_Click(sender As Object, e As EventArgs) Handles btnNext.Click
+        ' Clear data from array "a" (txtbxNArray)
+        For Each txtbx In txtbxNArray
+            txtbx.Text = ""
+        Next
+
+        ' Clear data from array "a" (txtbxOArray)
+        For Each txtbx In txtbxOArray
+            txtbx.Text = ""
+        Next
+
+        ' Enable array "b" (btnArray)
+        For Each btn In btnArray
+            btn.Enabled = True
+        Next
+
+        ' Clear txtbxSYSTRESULT
+        txtbxSYSTRESULT.Text = ""
+
+        ' Clear txtbxUSERRESULT
+        txtbxUSERRESULT.Text = ""
+
+        ' Generate random numbers for each button in array "b" (btnArray)
+        For Each btn In btnArray
+            Dim randomValue As Integer = rand.Next(1, 10) ' Range (1, 10)
+            btn.Text = randomValue.ToString()
+
+            ' Accumulate the random values to calculate the mathematical result
+            sysResult = randomValue
+        Next
+
+        ' Display the mathematical result in txtbxSYSTRESULT
+        txtbxSYSTRESULT.Text = sysResult.ToString()
+
+        ' Enabled btnSOLVE until btnNEXT is clicked again
+        btnSolve.Enabled = True
+
+        ' Enabled btnDELETE and btnDELETEALL
+        btnDelete.Enabled = True
+        btnDeleteAll.Enabled = True
+
+        ' Display btnNEXT
+        btnNext.Enabled = False
+    End Sub
+
+
+    Private Sub btnSOLVE_Click(sender As Object, e As EventArgs) Handles btnSolve.Click
+        ' Disable array "b" (btnArray)
+        For Each btn In btnArray
+            btn.Enabled = False
+        Next
+
+        ' Disable array "c" (btnPLUS, btnMINUS, btnFOR, btnDIVIDE)
+        btnPLUS.Enabled = False
+        btnMINUS.Enabled = False
+        btnFOR.Enabled = False
+        btnDIVIDE.Enabled = False
+
+        ' Disable btnDELETE and btnDELETEALL
+        btnDelete.Enabled = False
+        btnDeleteAll.Enabled = False
+
+        ' Check if there are any empty txtbxN or txtbxO elements
+        Dim isNEmpty As Boolean = False
+        Dim isOEmpty As Boolean = False
+
+        For Each txtbx In txtbxNArray
+            If txtbx.Text = "" Then
+                isNEmpty = True
+                Exit For
+            End If
+        Next
+
+        For Each txtbx In txtbxOArray
+            If txtbx.Text = "" Then
+                isOEmpty = True
+                Exit For
+            End If
+        Next
+
+        If Not isNEmpty AndAlso Not isOEmpty Then
+            ' Create lists to store numbers and operators
+            Dim numbers As New List(Of Double)
+            Dim operators As New List(Of String)
+
+            ' Initialize the first number
+            numbers.Add(Double.Parse(txtbxNArray(0).Text))
+
+            ' Process the expression from left to right
+            For i = 0 To txtbxNArray.Count - 2
+                operators.Add(txtbxOArray(i).Text)
+                numbers.Add(Double.Parse(txtbxNArray(i + 1).Text))
+                If operators.Count >= 1 Then
+                    If operators(0) = "+" Then
+                        numbers(0) += numbers(1)
+                    ElseIf operators(0) = "-" Then
+                        numbers(0) -= numbers(1)
+                    ElseIf operators(0) = "*" Then
+                        numbers(0) *= numbers(1)
+                    ElseIf operators(0) = "/" Then
+                        numbers(0) /= numbers(1)
+                    End If
+                    operators.RemoveAt(0)
+                    numbers.RemoveAt(1)
+                End If
+            Next
+
+            ' The result is in numbers(0)
+            Dim result As Double = numbers(0)
+
+            ' Display the result in txtbxUSERRESULT
+            txtbxUSERRESULT.Text = result.ToString()
+
+            ' Check if txtbxSYSTRESULT is equal to txtbxUSERRESULT
+            If sysResult = result Then
+                ' Increment TextBox10 count by 1
+                textBox10Count += 1
+                TextBox10.Text = textBox10Count.ToString()
+
+                ' Increase countdown time by 40 seconds
+                countdownTime += 40
+
+                ' Update TextBox11 with the new countdown time
+                TextBox11.Text = countdownTime.ToString()
+            End If
+
+            ' Enable btnNEXT to allow the user to proceed
+            btnNext.Enabled = True
+        Else
+            MessageBox.Show("Please fill all the boxes before solving.")
+        End If
+
+        ' Disable btnSOLVE until btnNEXT is clicked again
+        btnSolve.Enabled = False
+
+        ' Enable btnNEXT
+        btnNext.Enabled = True
+    End Sub
+
+
+    ' Helper function to Evaluacion Matematica expression
+    Private Function EvaluateExpression(expression As String) As Double
+        Dim table As New DataTable()
+        table.Columns.Add("expression", GetType(String), expression)
+        Dim row As DataRow = table.NewRow()
+        table.Rows.Add(row)
+        Return CDbl(row("expression"))
+    End Function
+
+End Class
